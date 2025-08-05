903f794a9936b6c0684209f7619ba427c22d24cb
AntonMenchaca <menchacanton@gmail.com>
2025-08-04 22:19:52 -0400

    add multiply and divide routes

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import Hello from "./hello.js"
+import Lab5 from "./Lab5/index.js";
 const app = express()
 Hello(app)
+Lab5(app)
 app.listen(process.env.PORT || 4000)
